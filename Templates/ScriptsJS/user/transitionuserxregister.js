4b6b1416c575e6ab68d1b22203dadd741b29fa70
Kevin Cano <kevin.cano@utp.edu.co>
2024-05-13 13:46:01 -0500

    3 Acercamiento al diseño - Registrar Usuario
    Se creo apartado resgistrar usuario(html,css)
    moficacion minimas y generales dle codigo para que funcionara correctamente registrar usuario, codigo basado en el iniciosesion.html

---
+++ Templates/ScriptsJS/user/transitionuserxregister.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Agregar un controlador de eventos a todos los enlaces con la clase "page-transition"
+    var pageLinks = document.querySelectorAll('.page-transition');
+    pageLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Evita la acción predeterminada del enlace
+            var href = this.getAttribute('href'); // Obtiene la URL de destino del enlace
+            // Realiza la animación de desvanecimiento
+            document.body.style.opacity = 0; // Desvanecer el contenido
+            setTimeout(function() {
+                window.location.href = href; // Redirigir a la página de destino
+            }, 200); // Espera 500 milisegundos (medio segundo) antes de redirigir
+        });
+    });
+});
